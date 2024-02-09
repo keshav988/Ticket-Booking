3712bda0ec04febb4b693e0712f46af2a30bbd4b
Jayanta Ojha <jayantaojha686@gmail.com>
2024-02-09 11:50:25 +0530

    backarrow of train.html is now working

--- train.js
+++ train.js
@@ -1,9 +1,14 @@
 const body = document.querySelector('body');
+const backArrow = document.querySelector('#back-arrow');
 const fromStation = document.querySelector('#from');
 const toStation = document.querySelector('#to');
 const journeyDate = document.querySelector('#journey-date');
 const journeyClass = document.querySelector('#journey-class');
 
+
+backArrow.addEventListener('click', () => {
+    window.location.href = 'index.html';
+})
 
 // Retrieve the query from local storage
 fromStation.innerText = localStorage.getItem('fromStation');
